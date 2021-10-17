1b5dad016a3b8be0f46e207425845d49b760fbe4
Leshawn Rice <leshawn.rice@outlook.com>
2021-10-17 14:57:12 -0700

    Updates

--- static/js/main.d8c1d56f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpkanoodle-king"]=this["webpackJsonpkanoodle-king"]||[]).push([[0],{11:function(n,e,t){"use strict";t.r(e);var c=t(1),o=t.n(c),i=t(3),r=t.n(i),s=(t(8),t(9),t(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Kanoodle King"})})};r.a.render(Object(s.jsx)(o.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(n,e,t){},9:function(n,e,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.d8c1d56f.chunk.js.map
